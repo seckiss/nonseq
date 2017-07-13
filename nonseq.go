1f44f963770595b67c0133aa2fecd92d4760dfb6
greg-sk <grzegorz.luczywo@securitykiss.com>
2017-07-13 23:34:20 +0200

    speck cipher

--- nonseq.go
+++ nonseq.go
@@ -1,7 +1,13 @@
 package nonseq
 
+import (
+	"fmt"
+
+	"github.com/ankitkalbande/simonspeck"
+)
+
 // Simon/Speck cipher key size mapping to block size. There are many variants
-// Block size/key length:
+// Block size/key length in bits:
 // - 32/64
 // - 48/72, 48/96
 // - 64/96, 64/128
@@ -11,8 +17,10 @@ package nonseq
 var keylen2blocksize = map[int]int{8: 4, 9: 6, 12: 8}
 
 type Generator struct {
-	key []byte
-	seq func() (seqid uint64, err error)
+	key       []byte
+	blocksize int
+	cipher    *simonspeck.Speck48Cipher
+	seq       func() (seqid uint64, err error)
 }
 
 // Secret key ensures a unique permutation of the input sequence, so that only someone who knows the key can guess nonseqid value
@@ -21,7 +29,12 @@ type Generator struct {
 // - for 32-bit block size the 32 most significant bits of nonseqid should be zero
 // - for 48-bit block size the 16 most significant bits of nonseqid should be zero
 func NewGenerator(key []byte, seq func() (seqid uint64, err error)) (*Generator, error) {
-	return nil, nil
+	if blocksize, pres := keylen2blocksize[len(key)]; pres {
+		cipher := simonspeck.NewSpeck48(key)
+		return &Generator{key, blocksize, cipher, seq}, nil
+	} else {
+		return nil, fmt.Errorf("Allowed key length is 8, 9 or 12 bytes")
+	}
 }
 
 func (g *Generator) Next() (seqid uint64, nonseqid uint64, err error) {
